0dc8b07cec0d3145a7c6f243f8b6b39db136e6ee
nurik91 <nurlibeknurmahanov@gmail.com>
2024-10-16 09:04:45 +0500

    configuration of CORS

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express'); // -	Чтобы подключит какие-то зависимости используем функцию “require” 
 const logger = require('morgan')
-// const bodyParser = require('body-parser'); // this
 const app = express(); // переменная которая использует фреймворк express
+const cors = require("cors")
 const passport = require('passport');
 
 
+
 app.use(logger('dev'))
-// app.use(bodyParser.urlencoded({ extended: true })); // this
-// app.use(bodyParser.json()); // this
+app.use(cors())
 app.use(express.urlencoded()) // для сериализации данных формата xml
 app.use(express.json()) // для сериализации данных формата json
 app.use(express.static(__dirname + "/public")) // чтобы файлы(картинки) были доступны в форнтенде
